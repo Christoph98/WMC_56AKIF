87b77845ae6b02f5e7f89b655aeff087490e0b35
Christop98 <WEG240780@spengergasse.at>
2025-09-13 16:43:24 +0200

    Mindestanforderungen gemacht

--- 00_Lab01_Wiederholung/script.js
+++ 00_Lab01_Wiederholung/script.js
@@ -2,9 +2,26 @@ const url = "https://opentdb.com/api.php?amount=1&category=18&difficulty=easy";
 
 document.getElementById('fetch-question').addEventListener('click', function () {
         const output = document.getElementById('Frage');
+        const output2 = document.getElementById('answers');
+        output2.innerHTML = '';
         fetch(url)
         .then(response => response.json())
         .then(data => {
         const question = data.results[0].question;
-        output.textContent = question;});
+        output.textContent = question;
+        const incorrect_answers = data.results[0].incorrect_answers, correct = data.results[0].correct_answer;
+        const combined = [...incorrect_answers, correct].sort(()=>Math.random()-0.5);
+        combined.forEach(element => {
+            const button = document.createElement('button');
+            button.textContent = element; 
+            output2.appendChild(button);
+            document.getElementById('answers').addEventListener('click',function (){
+            if (element === correct) {
+                button.style.backgroundColor = 'green';
+            } else {
+                button.style.backgroundColor = 'red';
+            }
+        });
+        });
     });
+});
