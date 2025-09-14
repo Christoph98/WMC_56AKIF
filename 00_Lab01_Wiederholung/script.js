dc483236192b4f026a14ce14dacd49a5b318eec4
Christoph Weghuber <WEG240780@spengergasse.at>
2025-09-14 12:02:28 +0200

    Kategorie hinzugefuegt

--- 00_Lab01_Wiederholung/script.js
+++ 00_Lab01_Wiederholung/script.js
@@ -1,15 +1,39 @@
-const url = "https://opentdb.com/api.php?amount=1&category=18&difficulty=easy";
+const baseurl = "https://opentdb.com/api.php?amount=1&category=18&difficulty=easy";
+const categoryUrl = "https://opentdb.com/api_category.php";
 let score = parseInt(localStorage.getItem("score")) || 0;
 
 function renderScore() {
     document.getElementById("score").textContent = score;
 }
 
+fetch(categoryUrl)
+.then(response => response.json())
+.then(data => {
+    const select = document.getElementById('category');
+    const allOptions = document.createElement('option');
+    allOptions.value = '';
+    allOptions.textContent = 'Alle Kategorien';
+    select.appendChild(allOptions);
+
+    data.trivia_categories.forEach(category => {
+        const option = document.createElement('option');
+        option.value = category.id;
+        option.textContent = category.name;
+        select.appendChild(option);
+    });
+});
+
+
 
 document.getElementById('fetch-question').addEventListener('click', function () {
         const output = document.getElementById('Frage');
         const output2 = document.getElementById('answers');
         output2.innerHTML = '';
+        const selectedCat = document.getElementById("category").value;
+        let url = baseurl;
+        if (selectedCat) {
+        url += `&category=${selectedCat}`;
+        }
         fetch(url)
         .then(response => response.json())
         .then(data => {
@@ -19,9 +43,9 @@ document.getElementById('fetch-question').addEventListener('click', function () 
         const combined = [...incorrect_answers, correct].sort(()=>Math.random()-0.5);
         let answered = false;
         combined.forEach(element => {
-            const button = document.createElement('button');
-            button.textContent = element; 
-            output2.appendChild(button);
+        const button = document.createElement('button');
+        button.textContent = element; 
+        output2.appendChild(button);
            button.addEventListener('click', function () {
                     if (answered) return;      
                     answered = true;           
